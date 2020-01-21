ca3792bf51c22754aed7df79f4a11b200cdcc9b5
Padma Kuruba <E004030@cigniti.com>
2020-01-21 16:58:57 +0530

    cqt39,cqt01

--- src/test/java/com/insight/SmartTest/Lib/HomeLib.java
+++ src/test/java/com/insight/SmartTest/Lib/HomeLib.java
@@ -3452,6 +3452,16 @@ public float getSellPriceFromInlineItemsContract(String contractid) throws Throw
 					"ZPML Minus ZDML Not equals the YP00 value", "", driver);
 		}
 	}
+	public void VerifyZPMLMinusZDMLShouldbeEqualToYP001(float ZPML, float ZDML, float YP00) throws Throwable {
+		float ZpmlminusZdml = ZPML - ZDML;
+		if ((YP00 == ZpmlminusZdml)) {
+			reporter.SuccessReport("Verify that ZPML minus  ZDML equals the YP00 value",
+					"ZPML Minus ZDML::" + ZpmlminusZdml + " equals the YP00 :" + YP00 + "value ", "");
+		} else {
+			reporter.failureReport("Verify that ZPML Minus ZDML equals the YP00 value",
+					"ZPML Minus ZDML Not equals the YP00 value", "", driver);
+		}
+	}
 
 	/**
 	 * 
