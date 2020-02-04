605c0bc7ff985c804e1e17b9ba3d0333132d65df
QTPSVC <QTPSVC@insight.com>
2020-02-04 12:36:59 -0500

    CCartLib

--- src/test/java/com/insight/Lib/CartLib.java
+++ src/test/java/com/insight/Lib/CartLib.java
@@ -462,12 +462,13 @@ public class CartLib extends ActionEngine {
 
 		if (isVisibleOnly(CartObj.SHIPMENT_NOTIFICATION, "ASN field")) {
 		String Email=getText(CartObj.SHIPMENT_NOTIFICATION,"Shipment Notification");
-		if(Email.contains(previousemail))
+		if(Email.contains(previousemail)) {
 			reporter.SuccessReport("Verify Defualt mail in ASN Field  Account Management - Account Tools Page", "In ASN Field is Defualt Email Exist as Expected",
-					"");
+					Email);
 		} else {
 			reporter.failureReport("erify Defualt mail in ASN Field   Account Management - Account Tools Page",
 					"ASN Field Email does not Exist", "", driver);
+		}
 		}
 	}
 
