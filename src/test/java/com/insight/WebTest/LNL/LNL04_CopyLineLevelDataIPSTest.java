e2b0ec55615dad7a382cd2fa51ee770f18fbc0a5
QTPSVC <QTPSVC@insight.com>
2020-02-05 23:55:41 -0700

    SLP lib test method change- added success report

--- src/test/java/com/insight/WebTest/LNL/LNL04_CopyLineLevelDataIPSTest.java
+++ src/test/java/com/insight/WebTest/LNL/LNL04_CopyLineLevelDataIPSTest.java
@@ -109,9 +109,9 @@ public class LNL04_CopyLineLevelDataIPSTest  extends LineLevelInfoLib{
 						searchLib.selectContract(data.get("Contract_Name2"));
 						// Verify contract page
 						pipLib.verifyWelcomePage();
-						// Search for another product >> Workstations
-						searchLib.searchInHomePage(data.get("SearchText1"));
-						searchLib.verifyTheResultsForSearchTerm(data.get("SearchText1"));
+						// Search for another product >> thinkpads
+						searchLib.searchInHomePage(data.get("SearchText3"));
+						searchLib.verifyTheResultsForSearchTerm(data.get("SearchText3"));
 						cartLib.selectFirstProductDisplay();
 						String mfrNumber4=prodLib.getInsightPartNumberInProductInfopage();
 						commonLib.addToCartAndVerify();
@@ -145,7 +145,7 @@ public class LNL04_CopyLineLevelDataIPSTest  extends LineLevelInfoLib{
 						/*clickOnLineLevelOptionalLinkByPartNum(mfrNumber2);
 						verifyDiversityPartnerexists(data.get("Diversity_Partner2"),mfrNumber2);*/
 						clickClearLink(mfrNumber1);
-						scrollUp();
+						//scrollUp();
 						//clickClearLink(mfrNumber3);
 						String reportingfield4= getReportingField4();
 						String reportingfield5= getReportingField5();
@@ -159,7 +159,7 @@ public class LNL04_CopyLineLevelDataIPSTest  extends LineLevelInfoLib{
 						// select second options from DD 
 						selectDiversityPartner(data.get("Diversity_Partner1"),mfrNumber1);
 						clickCopyToAllLink(mfrNumber1);
-						clickOnLineLevelOptionalLinkByPartNum(mfrNumber2);
+						//clickOnLineLevelOptionalLinkByPartNum(mfrNumber2);
 						verifyDiversityPartnerexists(data.get("Diversity_Partner1"),mfrNumber2);
 						clickOnLineLevelOptionalLinkByPartNum(mfrNumber3);
 						verifyDiversityPartnerexists(data.get("Diversity_Partner1"),mfrNumber3);
