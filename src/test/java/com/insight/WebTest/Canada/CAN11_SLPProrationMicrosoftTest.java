8cdd488eb62c501f27113d21042d05f0504045a7
qtpsvc <qtpsvc@USPHXWSEL02.Insight.com>
2020-02-07 12:23:16 -0500

    Fix for CAN11 as per review comments

--- src/test/java/com/insight/WebTest/Canada/CAN11_SLPProrationMicrosoftTest.java
+++ src/test/java/com/insight/WebTest/Canada/CAN11_SLPProrationMicrosoftTest.java
@@ -10,6 +10,9 @@ import com.insight.Lib.CanadaLib;
 import com.insight.Lib.CartLib;
 import com.insight.Lib.CommonLib;
 import com.insight.Lib.OrderLib;
+import com.insight.Lib.ProductDetailLib;
+import com.insight.Lib.ProductDisplayInfoLib;
+import com.insight.Lib.ShipBillPayLib;
 import com.insight.accelerators.ReportControl;
 import com.insight.accelerators.TestEngineWeb;
 import com.insight.googledrive.ReportStatus;
@@ -49,47 +52,81 @@ public class CAN11_SLPProrationMicrosoftTest extends CanadaLib {
 					CartLib cartLib = new CartLib();
 					OrderLib orderLib = new OrderLib();
 					CanadaLib canadaLib = new CanadaLib();
+					ShipBillPayLib shipbLib = new ShipBillPayLib();
+					ProductDisplayInfoLib prodinfo=new ProductDisplayInfoLib();
+					ProductDetailLib prodDetailLib=new ProductDetailLib();
 
-					cmtLib.loginToCMTSearchWebGrpAndUser(data.get("Header"), data.get("WebGrp"),
-							data.get("LnameEmailUname"), data.get("ContactName"));
+					//07-02 Updated latest flow
+					//cmtLib.loginToCMTSearchWebGrpAndUser(data.get("Header"), data.get("WebGrp"),
+					//		data.get("LnameEmailUname"), data.get("ContactName"));
+					cmtLib.loginToCMT(data.get("Header"));
+					cmtLib.searchForWebGroup(data.get("WebGrp"));
+					cmtLib.clickOnTheWebGroup(data.get("WebGrp_Name"));
+					cmtLib.hoverOnManageWebGroupsAndSelectOptions(data.get("Manage_Web_Grp_Options"));
+					cmtLib.searchForaUserAndSelect(data.get("LnameEmailUname"), data.get("contactName"));
+
 					cmtLib.clickOnloginAs();
 					switchToChildWindow();
-
+					//07-02-> Add Login Verification
+					cmtLib.loginVerification("User - "+data.get("contactName"));
 					canadaLib.CandaHomePageVerification();
+					
 					Thread.sleep(2000);
 					// SelectSoftwareLicAgrements
 					commonLib.clickOnAccountToolsAndClickOnProductGrp(data.get("Tools_Menu"),
 							data.get("Tools_Menu_DD"));
+					//07-02-> Add Page Verification of Microsoft Lic Agreement
+					canadaLib.verifySPLAPage();
 					canadaLib.selectProdCheckbox();
-					canadaLib.verifyProductsVisible();
 					// search product
 					commonLib.searchProduct(data.get("Productname"));
 					// commonLib.spinnerImage();
 					Thread.sleep(2000);
+					//07-02-> Verify product details
+					prodinfo.verifyTheManufacturerNumberInProductDetailsPage(data.get("Productname"));
+					//prodinfo.verifyProductDescAndPartNumberInCompanyStandards();
+					prodDetailLib.Getproductdetails();
+					// 07-02 Add Quantity 
+					prodinfo.enterQuantityOnProductDetailsPage(data.get("Quantity"));
 					commonLib.addToCartAndVerify();
 					canadaLib.verifyProductPrice();
 					String partNumber1 = cartLib.getPartNumber();
 					canadaLib.continueToCheckout();
-					// verify prorated price display
 					Thread.sleep(2000);
+					// 07-02 Add Cart page Verification and product details
+					cartLib.verifyCartPageAvailablity();
+					prodinfo.verifyCartPageAndPartDetailsForRecentlyItem();
 					canadaLib.verifyProratedPrice();
 					// proceed to checkout
 					orderLib.proceedToCheckout();
 					// ******** Click continue on Line level Info, Ship and Bill pay sections ********************//
 					orderLib.clickContinueOnLineLevelInfo();
+					//07-02 Verify Order and Item Info 
+					canadaLib.verifySBP();
 					orderLib.shippingBillPayContinueButton();
 					Thread.sleep(1000);
 					orderLib.shippingBillPayContinueButton();
 					Thread.sleep(2000);
 					// Select Terms- payment method and click Review Order
+					// 07-02 Need to verify PONumber value in TestData
 					orderLib.termsInPaymentInfo(data.get("PONumber"),data.get("POReleaseNumber"));
+					// 07-02 Need to check with ref with Report
+					String summaryAmount = cartLib.getSummaryAmountInCart();
+					shipbLib.getSummaryAmountsInCart(data.get("SubTotal"), data.get("Total"));
+					//orderLib.clickOnReviewOrderButton();  // Click Review order button
+					orderLib.verifyPlaceOrderLabel();
+					// Place Order
+					// 07-02 Add SubTotal Amount
+					orderLib.placeOrderAndVerifyReceiptOrderAndDate(summaryAmount);
+					shipbLib.getSummaryAmountsInCart(data.get("SubTotal"), data.get("Total"));
 
 					//orderLib.selectPaymentMethod(data.get("Payment_method"));
 					// Verify Ship Bill Page
 					Thread.sleep(2000);
 					canadaLib.veriyProratedPriceinSBP();
 					// verify subtotal(prorated price), verify receipt, order and date
-					String summaryAmount = cartLib.getSummaryAmountInCart();
+					shipbLib.clickOrderDetailsButtonInREceipt();
+					shipbLib.getSummaryAmountsInCart(data.get("SubTotal"), data.get("Total"));
 					commonLib.clickLogOutLink(data.get("Logout_Header"));
 					System.out.println("Test completed");
 					
