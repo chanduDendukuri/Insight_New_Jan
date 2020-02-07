29274d5f0e3abc407154afecb1a0528e2d889729
Bharathi Mettu <E002542@cigniti.com>
2020-02-07 16:48:59 +0530

    CRT23

--- src/test/java/com/insight/WebTest/Cart/CRT23_LoginFromCartPricingTest.java
+++ src/test/java/com/insight/WebTest/Cart/CRT23_LoginFromCartPricingTest.java
@@ -21,99 +21,108 @@ import com.insight.accelerators.TestEngineWeb;
 import com.insight.googledrive.ReportStatus;
 import com.insight.utilities.TestUtil;
 
-public class CRT23_LoginFromCartPricingTest extends CartLib{
+public class CRT23_LoginFromCartPricingTest extends CartLib {
 	CommonLib commonLib = new CommonLib();
 	CMTLib cmtLib = new CMTLib();
 	CartLib cartLib = new CartLib();
 	CanadaLib canadaLib = new CanadaLib();
 	SearchLib searchLib = new SearchLib();
 	ProductDisplayInfoLib prodInfoLib = new ProductDisplayInfoLib();
-	LineLevelInfoLib lineLevelLib=new LineLevelInfoLib();
+	LineLevelInfoLib lineLevelLib = new LineLevelInfoLib();
 	OrderLib orderLib = new OrderLib();
+
 	// #############################################################################################################
-    // #    Name of the Test         : CRT23_LoginFromCartPricing
-    // #    Migration Author         : Cigniti Technologies
-    // #
-    // #    Date of Migration        : August 2019
-    // #    DESCRIPTION              : This method is to perform Basic Cart  operations.
-    // #    Parameters               : StartRow ,EndRow , nextTestJoin
-    // #
-    // ###############################################################################################################	
-	@Parameters({"StartRow","EndRow","nextTestJoin"})
+	// # Name of the Test : CRT23_LoginFromCartPricing
+	// # Migration Author : Cigniti Technologies
+	// #
+	// # Date of Migration : August 2019
+	// # DESCRIPTION : This method is to perform Basic Cart operations.
+	// # Parameters : StartRow ,EndRow , nextTestJoin
+	// #
+	// ###############################################################################################################
+	@Parameters({ "StartRow", "EndRow", "nextTestJoin" })
 	@Test
-	public void Tc_CRT23(int StartRow,String EndRow,boolean nextTestJoin) throws Throwable {
-		
-					int counter = 0;
-					try {
-						int intStartRow = StartRow;
-						int intEndRow = ReportControl.fnGetEndRowCunt(EndRow, "CRT23_LoginFromCartPricing", TestDataInsight,
-								"Web_Cart");
-						for (int intCounter = intStartRow; intCounter <= intEndRow; intCounter++) {
-							try {
-								counter = intCounter;
-								fnOpenTest();
-								ReportStatus.fnDefaultReportStatus();
-								ReportControl.intRowCount = intCounter;
-								Hashtable<String, String> data = TestUtil.getDataByRowNo("CRT23_LoginFromCartPricing", TestDataInsight,
-										"Web_Cart", intCounter);
-								TestEngineWeb.reporter.initTestCaseDescription("LoginFromCartPricing");
-					
+	public void Tc_CRT23(int StartRow, String EndRow, boolean nextTestJoin) throws Throwable {
+
+		int counter = 0;
+		try {
+			int intStartRow = StartRow;
+			int intEndRow = ReportControl.fnGetEndRowCunt(EndRow, "CRT23_LoginFromCartPricing", TestDataInsight,
+					"Web_Cart");
+			for (int intCounter = intStartRow; intCounter <= intEndRow; intCounter++) {
+				try {
+					counter = intCounter;
+					fnOpenTest();
+					ReportStatus.fnDefaultReportStatus();
+					ReportControl.intRowCount = intCounter;
+					Hashtable<String, String> data = TestUtil.getDataByRowNo("CRT23_LoginFromCartPricing",
+							TestDataInsight, "Web_Cart", intCounter);
+					TestEngineWeb.reporter.initTestCaseDescription("LoginFromCartPricing");
+
 					commonLib.searchProduct(data.get("Search_Item"));
 					searchLib.verifyBreadCrumbInSearchResultsPage(data.get("Search_Item"));
 					prodInfoLib.getPartNumberInSearchResultsPage();
-					String listPrice=prodInfoLib.getFirtProductListPrice();
-					System.out.println("listPrice"+listPrice);
+					/* Review comment */ String listPrice = prodInfoLib.getFirtProductListPrice();
+					System.out.println("listPrice" + listPrice);
 					commonLib.addFirstDisplyedItemToCartAndVerify();
 					canadaLib.continueToCheckout();
 					cmtLib.handleWelcomeToInsightBetaPopUp();
 					commonLib.clickCart();
-					
+
 					canadaLib.verifyPlaceCartLabel();
 					prodInfoLib.verifyCartPageAndPartDetailsForRecentlyItem();
-					String summaryAmountNonLoggedIn=cartLib.getSummaryAmountInCart();
-					String currencyCode=cartLib.getCurrencyCode();
-					System.out.println("summaryAmountNonLoggedIn"+summaryAmountNonLoggedIn);
-					if(listPrice.equalsIgnoreCase(currencyCode +" "+summaryAmountNonLoggedIn)) {
-						reporter.SuccessReport("Verify the Non LoggedIn list price in cart", "list price same as in cart", summaryAmountNonLoggedIn);
-					}
-					else {
-						reporter.failureReport("Verify the Non LoggedIn list price in cart", "list price is not same as in cart", summaryAmountNonLoggedIn);
+					/* Review comment */String summaryAmountNonLoggedIn = cartLib.getSummaryAmountInCart();
+					String currencyCode = cartLib.getCurrencyCode();
+					System.out.println("summaryAmountNonLoggedIn" + summaryAmountNonLoggedIn);
+					if (listPrice.equalsIgnoreCase(currencyCode + " " + summaryAmountNonLoggedIn)) {
+						reporter.SuccessReport("Verify the Non LoggedIn list price in cart",
+								"list price same as in cart", currencyCode + " " + summaryAmountNonLoggedIn);
+					} else {
+						reporter.failureReport("Verify the Non LoggedIn list price in cart",
+								"list price is not same as in cart", summaryAmountNonLoggedIn);
 					}
 					orderLib.proceedToCheckout();
-					cmtLib.loginAsEndUserInMainPage(data.get("Header"),data.get("User_Name"),data.get("Password"));
-					
+					cmtLib.loginAsEndUserInMainPage(data.get("Header"), data.get("User_Name"), data.get("Password"));
+
 					orderLib.continueButtonOnAdditionalInformationSection();
-					String priceLogin=cartLib.getSummaryAmountInCart();
-					if(!(currencyCode +" "+priceLogin.trim()).equalsIgnoreCase(currencyCode +" "+summaryAmountNonLoggedIn.trim())) {
-						reporter.SuccessReport("Verify the Non LoggedIn and LoggedIn list price in SBP Page", "	LoggedIn price changed in SBP Page", "LoggedIn Price: "+priceLogin+"and Non-LoggedIn price: "+summaryAmountNonLoggedIn);
-					}
-					else {
-						reporter.failureReport("Verify the Non LoggedIn and LoggedIn list price in SBP Page", "	LoggedIn price same in SBP Page", "LoggedIn Price: "+priceLogin+"and Non-LoggedIn price: "+summaryAmountNonLoggedIn);
+					String priceLogin = cartLib.getSummaryAmountInCart();
+					if (!(currencyCode + " " + priceLogin.trim())
+							.equalsIgnoreCase(currencyCode + " " + summaryAmountNonLoggedIn.trim())) {
+						reporter.SuccessReport("Verify the Non LoggedIn and LoggedIn list price in SBP Page",
+								"	LoggedIn price changed in SBP Page",
+								"LoggedIn Price: " + currencyCode + " " + priceLogin + "and Non-LoggedIn price: "
+										+ currencyCode + " " + summaryAmountNonLoggedIn);
+					} else {
+						reporter.failureReport("Verify the Non LoggedIn and LoggedIn list price in SBP Page",
+								"	LoggedIn price same in SBP Page",
+								"LoggedIn Price: " + currencyCode + " " + priceLogin + "and Non-LoggedIn price: "
+										+ currencyCode + " " + summaryAmountNonLoggedIn);
 					}
 					commonLib.clickCart();
 					canadaLib.verifyPlaceCartLabel();
 					commonLib.emptyCartAndVerify();
 					System.out.println("Test completed");
 
-							} catch (Exception e) {
-								ReportStatus.blnStatus = false;
-								//gErrorMessage = e.getMessage();
-								gTestStatus = false;
-							}
-						}
-					} catch (Exception e) {
-						e.printStackTrace();
-						ReportStatus.blnStatus = false;
-						//gErrorMessage = e.toString();
-						gTestStatus = false;
-						ReportStatus.fnUpdateResultStatus("LoginFromCartPricing", "Tc_CRT23", ReportStatus.strMethodName, 1, browser);
-						throw new RuntimeException(e);
-					} finally {
-						ReportControl.fnEnableJoin();
-						ReportStatus.fnUpdateResultStatus("LoginFromCartPricing", "Tc_CRT23", ReportStatus.strMethodName, counter,
-								browser);
-						fnCloseTest();
-						ReportControl.fnNextTestJoin(nextTestJoin);
-					}
+				} catch (Exception e) {
+					ReportStatus.blnStatus = false;
+					// gErrorMessage = e.getMessage();
+					gTestStatus = false;
 				}
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			ReportStatus.blnStatus = false;
+			// gErrorMessage = e.toString();
+			gTestStatus = false;
+			ReportStatus.fnUpdateResultStatus("LoginFromCartPricing", "Tc_CRT23", ReportStatus.strMethodName, 1,
+					browser);
+			throw new RuntimeException(e);
+		} finally {
+			ReportControl.fnEnableJoin();
+			ReportStatus.fnUpdateResultStatus("LoginFromCartPricing", "Tc_CRT23", ReportStatus.strMethodName, counter,
+					browser);
+			fnCloseTest();
+			ReportControl.fnNextTestJoin(nextTestJoin);
+		}
+	}
+}
