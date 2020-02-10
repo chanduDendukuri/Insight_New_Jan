2dc0befe1546a5faf0952d424ac227abdd3f8c57
QTPSVC <QTPSVC@insight.com>
2020-02-10 12:25:28 -0500

    REP07

--- src/test/java/com/insight/Lib/CommonCanadaLib.java
+++ src/test/java/com/insight/Lib/CommonCanadaLib.java
@@ -479,7 +479,7 @@ public class CommonCanadaLib extends CommonCanadaPage {
             List<String> acutalContent = Arrays.asList(columnHeaders.split(","));
             System.out.println("Compare content" + downloadedExcelContent.equals(acutalContent));
             if (downloadedExcelContent.equals(acutalContent)) {
-                reporter.SuccessReport(""+columnHeaders, "columns are avilable in exportCart.xls", "columns: " + columnHeaders);
+                reporter.SuccessReport(""+columnHeaders, "columns are avilable in Excel File", "columns: " + columnHeaders);
             } else {
                 reporter.failureReport(""+columnHeaders, columnHeaders + " are not avilable", "", driver);
             }
