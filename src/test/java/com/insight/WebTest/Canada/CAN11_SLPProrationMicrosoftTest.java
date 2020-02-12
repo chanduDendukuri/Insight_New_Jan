f8789fdfd929dd621ca677d823412af1531f1da2
QTPSVC <QTPSVC@insight.com>
2020-02-12 08:59:26 -0700

    CAN-11 changes

--- src/test/java/com/insight/WebTest/Canada/CAN11_SLPProrationMicrosoftTest.java
+++ src/test/java/com/insight/WebTest/Canada/CAN11_SLPProrationMicrosoftTest.java
@@ -12,6 +12,7 @@ import com.insight.Lib.CommonLib;
 import com.insight.Lib.OrderLib;
 import com.insight.Lib.ProductDetailLib;
 import com.insight.Lib.ProductDisplayInfoLib;
+import com.insight.Lib.SLPLib;
 import com.insight.Lib.ShipBillPayLib;
 import com.insight.accelerators.ReportControl;
 import com.insight.accelerators.TestEngineWeb;
@@ -19,6 +20,16 @@ import com.insight.googledrive.ReportStatus;
 import com.insight.utilities.TestUtil;
 
 public class CAN11_SLPProrationMicrosoftTest extends CanadaLib {
+	
+	CommonLib commonLib = new CommonLib();
+	CMTLib cmtLib = new CMTLib();
+	CartLib cartLib = new CartLib();
+	OrderLib orderLib = new OrderLib();
+	CanadaLib canadaLib = new CanadaLib();
+	ShipBillPayLib shipbLib = new ShipBillPayLib();
+	ProductDisplayInfoLib prodinfo=new ProductDisplayInfoLib();
+	ProductDetailLib prodDetailLib=new ProductDetailLib();
+	SLPLib slpLib=new SLPLib();
 
 	// #############################################################################################################
 	// # Name of the Test : CAN11_SLPProrationMicrosoft
@@ -40,93 +51,87 @@ public class CAN11_SLPProrationMicrosoftTest extends CanadaLib {
 						int intEndRow = ReportControl.fnGetEndRowCunt(EndRow, "CAN11_SLPProrationMicrosoft", TestDataInsight, "Canada");
 						for (int intCounter = intStartRow; intCounter <= intEndRow; intCounter++) {
 							try {
-								counter = intCounter;
-								fnOpenTest();
-								ReportStatus.fnDefaultReportStatus();
-								ReportControl.intRowCount = intCounter;
-								Hashtable<String, String> data = TestUtil.getDataByRowNo("CAN11_SLPProrationMicrosoft", TestDataInsight,
-										"Canada", intCounter);
-								TestEngineWeb.reporter.initTestCaseDescription("SLPProrationMicrosoft");
-					CommonLib commonLib = new CommonLib();
-					CMTLib cmtLib = new CMTLib();
-					CartLib cartLib = new CartLib();
-					OrderLib orderLib = new OrderLib();
-					CanadaLib canadaLib = new CanadaLib();
-					ShipBillPayLib shipbLib = new ShipBillPayLib();
-					ProductDisplayInfoLib prodinfo=new ProductDisplayInfoLib();
-					ProductDetailLib prodDetailLib=new ProductDetailLib();
-
-					//07-02 Updated latest flow
-					//cmtLib.loginToCMTSearchWebGrpAndUser(data.get("Header"), data.get("WebGrp"),
-					//		data.get("LnameEmailUname"), data.get("ContactName"));
+					counter = intCounter;
+					fnOpenTest();
+					ReportStatus.fnDefaultReportStatus();
+					ReportControl.intRowCount = intCounter;
+					Hashtable<String, String> data = TestUtil.getDataByRowNo("CAN11_SLPProrationMicrosoft",
+							TestDataInsight, "Canada", intCounter);
+					TestEngineWeb.reporter.initTestCaseDescription("SLPProrationMicrosoft");
+					
 					cmtLib.loginToCMT(data.get("Header"));
 					cmtLib.searchForWebGroup(data.get("WebGrp"));
 					cmtLib.clickOnTheWebGroup(data.get("WebGrp_Name"));
 					cmtLib.hoverOnManageWebGroupsAndSelectOptions(data.get("Manage_Web_Grp_Options"));
-					cmtLib.searchForaUserAndSelect(data.get("LnameEmailUname"), data.get("contactName"));
-
-					cmtLib.clickOnloginAs();
-					switchToChildWindow();
-					//07-02-> Add Login Verification
-					cmtLib.loginVerification("User - "+data.get("contactName"));
+					cmtLib.searchForaUserAndSelect(data.get("LnameEmailUname"), data.get("ContactName"));
+					cmtLib.loginAsAdminCMT();
+					// Login verification
+					cmtLib.loginVerification("User - "+data.get("ContactName"));
 					canadaLib.CandaHomePageVerification();
 					
 					Thread.sleep(2000);
 					// SelectSoftwareLicAgrements
 					commonLib.clickOnAccountToolsAndClickOnProductGrp(data.get("Tools_Menu"),
 							data.get("Tools_Menu_DD"));
-					//07-02-> Add Page Verification of Microsoft Lic Agreement
-					canadaLib.verifySPLAPage();
-					canadaLib.selectProdCheckbox();
+					// Select Software  Lic Agreements
+			     	canadaLib.selectSPLADetailsProductCheckBox(data.get("SPLA"));
+					// verify search results and select first product
+			     	slpLib.verifysearchResultsPageForSLP();
 					// search product
 					commonLib.searchProduct(data.get("Productname"));
-					// commonLib.spinnerImage();
 					Thread.sleep(2000);
-					//07-02-> Verify product details
+					// Verify product details
 					prodinfo.verifyTheManufacturerNumberInProductDetailsPage(data.get("Productname"));
 					//prodinfo.verifyProductDescAndPartNumberInCompanyStandards();
 					prodDetailLib.Getproductdetails();
-					// 07-02 Add Quantity 
+					// Add Quantity 
 					prodinfo.enterQuantityOnProductDetailsPage(data.get("Quantity"));
+					String price = slpLib.getpricefromProductdetailpage().replace("CAD", "").replace("$", "").replace(",", ""); 
+					Double Actualprice = Double.valueOf(price);
 					commonLib.addToCartAndVerify();
-					canadaLib.verifyProductPrice();
+					
 					String partNumber1 = cartLib.getPartNumber();
 					canadaLib.continueToCheckout();
 					Thread.sleep(2000);
-					// 07-02 Add Cart page Verification and product details
+					//Add Cart page Verification and product details
 					cartLib.verifyCartPageAvailablity();
 					prodinfo.verifyCartPageAndPartDetailsForRecentlyItem();
-					canadaLib.verifyProratedPrice();
+					String subTotal=shipbLib.getTotalAmountInCart(data.get("SubTotal"));
+				    Float subTotalAmount = Float.parseFloat(subTotal.replace("$", "").replace(",", ""));
+				     // verifying Prorated Price of product in cart
+				   slpLib.verifyProrationincartpage(data.get("Productname"), Actualprice);
+				    // verifying Prorated Price with the subtotal price in cart price
+				   slpLib.verifyProrationPrice(subTotalAmount, Actualprice);
 					// proceed to checkout
 					orderLib.proceedToCheckout();
 					// ******** Click continue on Line level Info, Ship and Bill pay sections ********************//
 					orderLib.clickContinueOnLineLevelInfo();
-					//07-02 Verify Order and Item Info 
-					canadaLib.verifySBP();
-					orderLib.shippingBillPayContinueButton();
-					Thread.sleep(1000);
-					orderLib.shippingBillPayContinueButton();
-					Thread.sleep(2000);
-					// Select Terms- payment method and click Review Order
-					// 07-02 Need to verify PONumber value in TestData
-					orderLib.termsInPaymentInfo(data.get("PONumber"),data.get("POReleaseNumber"));
-					// 07-02 Need to check with ref with Report
+					// Verify Order and Item Info 
+					 canadaLib.verifySBP();
+					 orderLib.shippingBillPayContinueButton();// shipping address continue button
+                     orderLib.billingAddressContinueButton();  // Billing address continue button
+                     orderLib.selectPaymentInfoMethodCreditCard(data.get("cardNumber"), data.get("cardName"), data.get("month"), data.get("year"),data.get("poNumebr"),data.get("POReleaseNumber"));
+				     orderLib.clickOnReviewOrderButton();
+					
+					//  Need to check with ref with Report
 					String summaryAmount = cartLib.getSummaryAmountInCart();
-					shipbLib.getSummaryAmountsInCart(data.get("SubTotal"), data.get("Total"));
-					//orderLib.clickOnReviewOrderButton();  // Click Review order button
-					orderLib.verifyPlaceOrderLabel();
-					// Place Order
-					// 07-02 Add SubTotal Amount
+					//shipbLib.getSummaryAmountsInCart(data.get("SubTotal"), data.get("Total"));
+					
+					String totalAmt=shipbLib.getTotalAmountInCart(data.get("Total"));
+					Double totalAmount = Double.valueOf(totalAmt.replace("$", "").replace(",", ""));
+					slpLib.verifyProrationPrice(Actualprice, totalAmount);
+					// place requisition
 					orderLib.placeOrderAndVerifyReceiptOrderAndDate(summaryAmount);
-					shipbLib.getSummaryAmountsInCart(data.get("SubTotal"), data.get("Total"));
-
-					//orderLib.selectPaymentMethod(data.get("Payment_method"));
-					// Verify Ship Bill Page
-					Thread.sleep(2000);
-					canadaLib.veriyProratedPriceinSBP();
-					// verify subtotal(prorated price), verify receipt, order and date
-					shipbLib.clickOrderDetailsButtonInREceipt();
-					shipbLib.getSummaryAmountsInCart(data.get("SubTotal"), data.get("Total"));
+					//Verify Receipt page
+					shipbLib.getReferenceNum();
+					//Verify Receipt
+					orderLib.verifyReceiptVerbiage();
+					orderLib.clickOrderDetailsLinkOnReceiptPage();
+					
+					// Verify prorated price in receipt page
+					String totalAmt_receiptPage=shipbLib.getTotalAmountInCart(data.get("Total"));
+					Double totalAmount_receiptPage = Double.valueOf(totalAmt_receiptPage.replace("$", "").replace(",", ""));
+					slpLib.verifyProrationPrice(Actualprice, totalAmount_receiptPage);
 					commonLib.clickLogOutLink(data.get("Logout_Header"));
 					System.out.println("Test completed");
 					
