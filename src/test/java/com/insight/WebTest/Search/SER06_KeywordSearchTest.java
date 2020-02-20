413355bc6d4e7c9211e47f21ec8ada1ac11c6182
QTPSVC <QTPSVC@insight.com>
2020-02-20 00:00:31 -0500

    Ser-06 JS click change

--- src/test/java/com/insight/WebTest/Search/SER06_KeywordSearchTest.java
+++ src/test/java/com/insight/WebTest/Search/SER06_KeywordSearchTest.java
@@ -75,7 +75,7 @@ public class SER06_KeywordSearchTest extends SearchLib {
 					verifyTheResultsForSearchTerm(data.get("SearchText4"));
 					// in-stock filter verification
 					verifyFilterBreadCrumb(data.get("In_Stock_Only"));
-					scrollToBottomWithCordinate("300");
+					scrollToBottomWithCordinate("600");
 					// Approve items only
 					filterSelectionInProductsSearchPage(data.get("Approved_Items"));
 					verifyBreadCrumbInSearchResultsPage(data.get("Approved_Items_Remove_Filter"));
